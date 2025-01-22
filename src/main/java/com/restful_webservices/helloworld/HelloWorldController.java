59891854c320d6f91d938db68f3caaf23ed62aff
dhruthinooka <dhruthi74@gmail.com>
2025-01-22 12:22:47 +0100

    RequestMapping

---
+++ src/main/java/com/restful_webservices/helloworld/HelloWorldController.java
@@ -0,0 +1,20 @@
+package com.restful_webservices.helloworld;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+//REST API
+@RestController
+public class HelloWorldController {
+	
+	// /hello-world
+	@RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+	public String helloWorld() {
+		
+		return "Hello World";
+		
+		
+	}
+
+}
---
+++ src/test/java/com/restful_webservices/RestfulWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restful_webservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
