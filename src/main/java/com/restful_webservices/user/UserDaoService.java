df504631f80ee1b1578ac448371fd522d907e3dd
dhruthinooka <dhruthi74@gmail.com>
2025-01-23 11:54:28 +0100

    POST method for creating a User with status code & location

--- src/main/java/com/restful_webservices/user/UserDaoService.java
+++ src/main/java/com/restful_webservices/user/UserDaoService.java
@@ -13,10 +13,12 @@ public class UserDaoService {
 	// static user list
 	private static List<User> users = new ArrayList<>();
 	
+	private static int usersCount = 0;
+	
 	static {
-		users.add(new User(1, "Daniel", LocalDate.now().minusYears(30)));
-		users.add(new User(2, "Jim", LocalDate.now().minusYears(25)));
-		users.add(new User(3, "Corbett", LocalDate.now().minusYears(23)));
+		users.add(new User(++usersCount, "Daniel", LocalDate.now().minusYears(30)));
+		users.add(new User(++usersCount, "Jim", LocalDate.now().minusYears(25)));
+		users.add(new User(++usersCount, "Corbett", LocalDate.now().minusYears(23)));
 	}
 	
 	public List<User> findAll() {
@@ -28,6 +30,12 @@ public class UserDaoService {
 		Predicate<? super User> predicate = user -> user.getId().equals(id);
 		return users.stream().filter(predicate).findFirst().get();
 		
+	}
+	
+	public User save(User user) {
+		user.setId(++usersCount);
+		users.add(user);
+		return user;
 	}
 
 }
--- src/main/java/com/restful_webservices/user/UserResource.java
+++ src/main/java/com/restful_webservices/user/UserResource.java
@@ -1,10 +1,16 @@
 package com.restful_webservices.user;
 
+import java.net.URI;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.ResponseEntity.BodyBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -27,6 +33,17 @@ public class UserResource {
 	@GetMapping("/users/{id}")
 	public User retrieveUser(@PathVariable int id) {
 		return service.findOne(id);
+	}
+	
+	// POST /users
+	@PostMapping("/users")
+	public ResponseEntity<User> createUser(@RequestBody User user) {
+		User savedUser = service.save(user);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(savedUser.getId())
+				.toUri();
+		return ResponseEntity.created(location).build();
 	}
 
 }
