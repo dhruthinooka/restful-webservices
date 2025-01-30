f96dadadc125c2b12b3f2228dc74940103086c7f
dhruthinooka <dhruthi74@gmail.com>
2025-01-30 15:29:50 +0100

    Created Post Entity, @ManyToOne, @OneToMany relations

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,8 @@
 insert into user_details(id, birth_date, name) values(10001, current_date(), 'Dhruthi');
 insert into user_details(id, birth_date, name) values(10002, current_date(), 'Krithi');
 insert into user_details(id, birth_date, name) values(10003, current_date(), 'Shruthi');
+
+insert into post(id, description, user_id) values(20001, 'I want to learn AWS', 10001);
+insert into post(id, description, user_id) values(20002, 'I want to learn DevOPs', 10001);
+insert into post(id, description, user_id) values(20003, 'I want to get Azure Certification', 10002);
+insert into post(id, description, user_id) values(20004, 'I want to learn Multi Cloud', 10002);
